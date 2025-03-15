63e7c6a0e2d356d0aac0c01030f6546b8c0a4cf3
unknown <ihzhanauvalb@gmail.com>
2025-03-16 02:56:00 +0700

    refactor: output dari fungsi convertLength dan isEven

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import convertLength from "./convertLength.js";
 import convertFahrenheitToCelsius from "./fahrenheitToCelsius.js";
+import isEven from "./isEven.js";
 
 // Penggunaan convertFahrenheitToCelsius
 let fahrenheitInput = 200; // Misalnya suhu 100°F
 let celsiusOutput = convertFahrenheitToCelsius(fahrenheitInput);
 console.log(`${fahrenheitInput}°F = ${celsiusOutput.toFixed(2)}°C`);
+
+// Penggunaan convertLength
+console.log(convertLength(100000)); // ouput "1 km"
+console.log(convertLength(2)); // Output: "200000 cm"
+
+// Penggunaan isEven
+// Contoh penggunaan
+console.log(`1000 → isEven: ${isEven(1000)}`); // Output: 1000 => isEven: true
+console.log(`1001 → isEven: ${isEven(1001)}`); // Output: 1001 => isEven: false
